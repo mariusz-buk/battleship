1e773b59aa521d0a34d9a77b9cfbb0c5e5625f34
Mariusz Buk <mariusz@buk.email>
2020-06-19 09:17:45 +0200

    moved some parts to go template

--- battleship.go
+++ battleship.go
@@ -1,26 +1,43 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 type command struct {
 	name string
-	fn   func()
+	fn   func(http.ResponseWriter, *http.Request)
+}
+
+type player struct {
+	token string
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
-func getConnectionToken() {
+func addTemplateIterator() {
+	template.FuncMap{
+		"Iterate": func(count *uint) []uint {
+			var i uint
+			var Items []uint
+			for i = 0; i < (*count); i++ {
+				Items = append(Items, i)
+			}
+			return Items
+		},
+	}
+}
+
+func getConnectionToken(writer http.ResponseWriter, request *http.Request) {
 
 }
 
-func main() {
+func addCommands() {
 	commands := []command{
 		{
 			name: "getConnectionToken",
@@ -28,7 +45,12 @@ func main() {
 		},
 	}
 	for _, command := range commands {
-		http.HandleFunc("/"+command.name, command.fn())
+		http.HandleFunc("/"+command.name, command.fn)
 	}
+}
+
+func main() {
+	addTemplateIterator()
+	addCommands()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
